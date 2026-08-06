537a1e9d6fd70f578edb4a217e51e890d81e74c0
fwoffo-sys <fwoffo@gmail.com>
2026-08-06 11:24:17 -0400

    added a recursive copy directory function to main.

--- src/main.py
+++ src/main.py
@@ -1,15 +1,24 @@
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode
+import os
+import shutil
+
+def copy_directory(src, dest):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    if os.path.isdir(src):
+        os.mkdir(dest)
+    source_contents = os.listdir(src)
+    for content in source_contents:
+        src_path = os.path.join(src, content)
+        dest_path = os.path.join(dest, content)
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dest_path)
+        else:
+            copy_directory(src_path, dest_path)
+
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node)
-    properties = {
-    "href": "https://www.google.com",
-    "target": "_blank",
-    }
-    htmlNode = HTMLNode(tag="p", value="blahblahblah", props=properties)
-    print(htmlNode)
-    print(htmlNode.props_to_html())
+    copy_directory("./static", "./public")
 
 main()
